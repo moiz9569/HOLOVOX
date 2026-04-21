e6f5625adca4facbd692e3aef400cdd7f6b01d65
EmadAliKhan <emadalikhan5@gmail.com>
2026-04-21 16:49:51 +0500

    working

---
+++ src/app/models/Request.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const RequestSchema = new mongoose.Schema({
+  sender: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  receiver: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ["pending", "accepted", "rejected"],
+    default: "pending",
+  },
+}, { timestamps: true });
+
+
+const RequestModel =
+  mongoose.models.Request || mongoose.model("Request", RequestSchema);
+
+export default RequestModel;
--- src/app/models/User.model.js
+++ src/app/models/User.model.js
@@ -21,7 +21,7 @@ const UserSchema = new mongoose.Schema(
     },
     role: {
       type: String,
-      enum: ["user", "admin"],
+      enum: ["user", "admin","doctor","lawyer"],
       default: "user",
     },
     verified:{
